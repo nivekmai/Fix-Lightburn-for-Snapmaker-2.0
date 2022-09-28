e1aafd07f5e665225fe1345a0d3cd1c65c4859ba
Kevin Christensen <nivekmai@gmail.com>
2022-09-28 05:42:45 +0000

    Update fix_nc.sh

--- fix_nc.sh
+++ fix_nc.sh
@@ -3,9 +3,9 @@
 FILE=$1
 OUT=`echo "$FILE" |  sed 's/\(.*\)\(\..*\)/\1 - fixed\2/'`
 sed '/M[678]/d' "$FILE" \
-	| sed 's/M05/M05 I/g' \
+	| sed 's/M05/G1 I S0/g' \
 	| tac \
-	| sed '0,/M05 I/{s/M05 I/M05/}' \
+	| sed '0,/G1 I S0/{s/G1 I S0/M05/}' \
 	| tac \
 	| (echo "M3 S0" && cat) \
 	> "$OUT"
