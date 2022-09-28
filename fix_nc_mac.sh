30a94a3d7c41c1c929df73c1ab684dab318d8272
Kevin Christensen <nivekmai@gmail.com>
2022-09-28 05:43:21 +0000

    Update fix_nc_mac.sh

--- fix_nc_mac.sh
+++ fix_nc_mac.sh
@@ -3,9 +3,9 @@
 FILE=$1
 OUT=`echo "$FILE" |  sed 's/\(.*\)\(\..*\)/\1 - fixed\2/'`
 sed '/M[678]/d' "$FILE" \
-	| sed 's/M05/M05 I/g' \
+	| sed 's/M05/G1 I S0/g' \
 	| tail -r \
-	| awk '/M05 I/ && !done { gsub(/M05 I/, "M05"); done=1;}; 1;' \
+	| awk '/G1 I S0/ && !done { gsub(/G1 I S0/, "M05"); done=1;}; 1;' \
 	| tail -r \
 	| (echo "M3 S0" && cat) \
 	> "$OUT"
